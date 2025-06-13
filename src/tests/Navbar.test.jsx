1bf0274318e0b218087352ae282110ad664a5d48
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-06-13 20:08:04 +0530

    new test added with boilerplate

---
+++ src/tests/Navbar.test.jsx
@@ -0,0 +1,9 @@
+import { describe,test, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Navbar from "../component/Navbar.jsx";
+
+describe("Navbar component",() => {
+    it("navbar renders",() => {
+
+    })
+})
