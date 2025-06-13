8d85d19250cb7120935a2c878951b376734bd8dd
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-06-13 15:39:27 +0530

    basic route connectivity added for testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import "./App.css";
+import { Link } from "react-router-dom";
 
 function App() {
-    return <></>;
+    return(
+        <div>
+            <Link to="checkout">
+                <button >
+                    Go to the checkout page.
+                </button>
+            </Link>
+            <h1>This is the home page</h1>
+            <Link to="item">
+                <button>
+                    Go to the item page.
+                </button>
+            </Link>
+        </div>
+    );
 }
 
 export default App;
