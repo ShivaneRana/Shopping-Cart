dbd674ea935704680af59d6102b07d28a04fe743
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-06-13 15:39:56 +0530

    created for storing all the routes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,9 +1,21 @@
 import App from "./App.jsx";
+import Checkout from "./component/Checkout.jsx";
+import ItemPage from "./component/ItemPage.jsx";
+import ErrorPage from "./component/ErrorPage.jsx";
 
 const Routes = [
     {
         path:"/",
         element:<App></App>,
+        errorElement:<ErrorPage></ErrorPage>,
+    },
+    {
+        path:"checkout",
+        element:<Checkout></Checkout>
+    },
+    {
+        path:"item",
+        element:<ItemPage></ItemPage>,
     }
 ];
 
