f739128fe3ee3b5d81b3221cb9855caa3920e49c
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-06-14 16:20:43 +0530

    1. two div added for left and right side of the navbar 2. Link attribute imported from react-router-dom for future usecase

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,9 +1,38 @@
 import style from "../style/Navbar.module.css";
+import { Link } from "react-router-dom";
 
 function Navbar(){
     return(
-        <div>
-
+        <div className={style.main}>
+            <div className={style.leftside}>
+                <button>
+                    <h1>
+                        EdenCrate.
+                    </h1>
+                </button>
+                <button>
+                    <Link>
+                        Home
+                    </Link>
+                </button>
+                <button>
+                    <Link>
+                        Store
+                    </Link>
+                </button>
+            </div>
+            <div className={style.rightside}>
+                <label htmlFor="searchFruit"></label>
+                <input type="text" name="searchFruit" placeholder="Search"></input>
+                <div>
+                    <button>
+                        <img>
+                        </img>
+                        <img>
+                        </img>
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
