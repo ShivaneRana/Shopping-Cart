90088fbed1fc2e85044293b8cff74da40985760b
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-06-14 20:57:40 +0530

    basic structure of the navbar created

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,36 +1,78 @@
 import style from "../style/Navbar.module.css";
+import favIconClicked from "../assets/images/fav.png";
+import favIconNotClicked from "../assets/images/notfav.png";
+import cartIcon from "../assets/images/cart.png";
 import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
         <div className={style.main}>
             <div className={style.leftside}>
-                <button>
-                    <h1>EdenCrate.</h1>
-                </button>
-                <button>
-                    <Link>Home</Link>
-                </button>
-                <button>
-                    <Link>Store</Link>
-                </button>
+                <h1>EdenCrate.</h1>
+                <div>
+                    <HomeButton></HomeButton>
+                    <StoreButton></StoreButton>
+                </div>
             </div>
             <div className={style.rightside}>
-                <label htmlFor="searchFruit"></label>
-                <input
-                    type="text"
-                    name="searchFruit"
-                    placeholder="Search"
-                ></input>
+                <SearchField></SearchField>
                 <div>
-                    <button>
-                        <img></img>
-                        <img></img>
-                    </button>
+                    <FavouriteButton></FavouriteButton>
+                    <CartButton></CartButton>
                 </div>
             </div>
         </div>
     );
+}
+
+function HomeButton(){
+    return(
+        <button>
+            <Link>
+                Home
+            </Link>
+        </button>
+    )
+}
+
+function StoreButton(){
+    return(
+        <button>
+            <Link>
+                Store
+            </Link>
+        </button>
+    )
+}
+
+function SearchField(){
+    return(
+        <>
+        <label htmlFor="searchFruit"></label>
+        <input
+            type="text"
+            name="searchFruit"
+            placeholder="Search"
+        >
+        </input>
+        </>
+    )
+}
+
+function FavouriteButton(){
+    return(
+        <button>
+            <img src={favIconNotClicked}></img>
+        </button>
+    )
+}
+
+function CartButton(){
+    return(
+        <button>
+            <img src={cartIcon}></img>
+        </button>
+    )
 }
 
 export default Navbar;
