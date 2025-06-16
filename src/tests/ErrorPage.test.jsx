aea18688e98453fa5eca2d8585dd549b66ef62e0
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-06-16 15:15:00 +0530

    test added for errorPage component

--- src/tests/ErrorPage.test.jsx
+++ src/tests/ErrorPage.test.jsx
@@ -1,11 +1,40 @@
-import { describe,it, expect } from "vitest";
+import { describe,it, expect, beforeEach } from "vitest";
 import { render, screen} from "@testing-library/react";
+import { createMemoryRouter, RouterProvider} from "react-router-dom";
 import ErrorPage from "../component/ErrorPage.jsx";
 
 describe("ErrorPage component",() => {
-    it("ErrorPage is rendering",() => {
-        render(<ErrorPage></ErrorPage>);
+    beforeEach(() => {
+        const route = createMemoryRouter(
+            [
+                {
+                    path:"/",
+                    element:null,
+                    errorElement:<ErrorPage></ErrorPage>,
+                },
 
-        expect(screen.get)
+            ],
+            {
+                initialEntries:['/bad-route'],
+            }
+        )
+
+        render(<RouterProvider router={route}/>)
+    })
+
+    it("Heading is rendering",() => {
+        expect(screen.getByRole("heading",{name: /Whoops/i })).toBeInTheDocument();
+    })
+
+    it("image is rendering", () => {
+        const img = (screen.getByAltText("errorIcon"));
+        expect(img).toBeInTheDocument();
+        expect(img).toHaveAttribute("src",expect.stringContaining("error.png"));
+    })
+
+    it("Link is rendering",async () => {
+        const link = (screen.getByRole("link",{name: /Go to home page/i}));
+        expect(link).toBeInTheDocument();
+        expect(link).toHaveAttribute("href","/Shopping-Cart");
     })
 })
