bee334976f15d5d036cb9afa69ebdb08665e3e73
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-06-16 13:56:51 +0530

    removed

--- src/component/ItemPage.jsx
+++
@@ -1,8 +0,0 @@
-import style from "../style/ItemPage.module.css";
-import { Link } from "react-router-dom";
-
-function ItemPage() {
-    return <div className={style.main}></div>;
-}
-
-export default ItemPage;
