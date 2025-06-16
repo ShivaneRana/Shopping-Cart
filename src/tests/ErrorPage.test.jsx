b283afaa18964f2245c2761d361641b2d572a6f8
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-06-16 15:56:19 +0530

    added a test to check if the link attribute is working properly

--- src/tests/ErrorPage.test.jsx
+++ src/tests/ErrorPage.test.jsx
@@ -1,6 +1,7 @@
 import { describe,it, expect, beforeEach } from "vitest";
 import { render, screen} from "@testing-library/react";
-import { createMemoryRouter, RouterProvider} from "react-router-dom";
+import {MemoryRouter, createMemoryRouter, RouterProvider, Routes, Route} from "react-router-dom";
+import userEvent from "@testing-library/user-event";
 import ErrorPage from "../component/ErrorPage.jsx";
 
 describe("ErrorPage component",() => {
@@ -36,5 +37,31 @@ describe("ErrorPage component",() => {
         const link = (screen.getByRole("link",{name: /Go to home page/i}));
         expect(link).toBeInTheDocument();
         expect(link).toHaveAttribute("href","/Shopping-Cart");
+    })
+
+    it("check if the link button works", async () => {
+        const user = userEvent.setup();
+
+        const route = createMemoryRouter([
+            {
+                path:"/error",
+                element:<ErrorPage></ErrorPage>,
+            },
+            {
+                path:"/shopping-cart",
+                element:<div>home page</div>
+            },
+        ],
+        {
+            initialEntries:["/error"],
+        })
+
+        render(<RouterProvider router={route}/>)
+
+        const link = screen.getAllByRole("link", {name: /home page/i});
+        await user.click(link[0])
+
+        const para = screen.getAllByText(/home page/i);
+        expect(para[0]).toBeInTheDocument();
     })
 })
