6a39d99da8b61398b174b9f59455088876940607
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-06-16 13:48:43 +0530

    1. added content to show what error has occurred 2. added a button to redirect to home page 3. log the error in console

--- src/component/ErrorPage.jsx
+++ src/component/ErrorPage.jsx
@@ -1,12 +1,29 @@
-import { Link } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
+import errorIcon from "../assets/images/error.png"
 import style from "../style/ErrorPage.module.css";
 
 function ErrorPage() {
+    const error = useRouteError();
+    console.error(error.statusText);
+    
     return (
         <div className={style.main}>
-            <h1>Oh no! an error has occurred.</h1>
+            <img src={errorIcon}></img>
+            <h2>Whoops... An error has occurred</h2>
+            <i>{error.statusText || error.message}</i>
+            <GoHomeButton></GoHomeButton>
         </div>
     );
+}
+
+function GoHomeButton(){
+    return(
+        <Link to="/Shopping-Cart">
+            <button>
+                Go to home page
+            </button>
+        </Link>
+    )
 }
 
 export default ErrorPage;
