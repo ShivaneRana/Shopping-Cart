8d375eda265541f89d29cc73f85e4027c800b139
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-06-17 21:48:18 +0530

    now pass values to child compeonent using context api

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import style from "./App.module.css";
 import Navbar from "./component/Navbar.jsx";
+import { createContext } from "react";
 import { Outlet } from "react-router-dom";
+import { useFavoriteState } from "./component/Utility.jsx";
+
+export let context = createContext();
 
 function App() {
+    const {favourite,toggleFavourite} = useFavoriteState();
+
     return (
         <div className={style.main}>
-            <Navbar></Navbar>
-            <Outlet></Outlet>
+            <context.Provider value={{
+                favourite,
+                toggleFavourite
+            }}>
+                <Navbar></Navbar>
+                <Outlet></Outlet>
+            </context.Provider>
         </div>
     );
 }
