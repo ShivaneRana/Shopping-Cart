11175e6fb530a6ec69eb3d7445e7ed319910d31c
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-06-17 14:40:04 +0530

    new routes added as children of root path

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,11 +1,28 @@
 import App from "./App.jsx";
+import CheckoutPage from "./component/CheckoutPage.jsx";
 import ErrorPage from "./component/ErrorPage.jsx";
+import HomePage from "./component/HomePage.jsx";
+import StorePage from "./component/StorePage.jsx";
 
 const Routes = [
     {
         path: "/Shopping-Cart",
         element: <App></App>,
         errorElement: <ErrorPage></ErrorPage>,
+        children:[
+            {
+                index: true,
+                element: <HomePage></HomePage>,
+            },
+            {
+                path: "Store",
+                element: <StorePage></StorePage>,
+            },
+            {
+                path: "Checkout",
+                element:<CheckoutPage></CheckoutPage>,
+            }
+        ]
     },
     {
         path: "*",
