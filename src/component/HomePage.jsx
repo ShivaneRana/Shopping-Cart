04d3bec09617f6a95a913c8558ae03ccb280a6dc
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-06-17 15:51:26 +0530

    basic code added

--- src/component/HomePage.jsx
+++ src/component/HomePage.jsx
@@ -1,11 +1,49 @@
+import { Link } from "react-router-dom";
 import style from "../style/HomePage.module.css";
 
 function HomePage() {
     return (
         <div className={style.main}>
-            <h1>This is home page</h1>
+            <WelcomeText></WelcomeText>
+            <ShopButton></ShopButton>
+            <Carousel></Carousel>
         </div>
     );
 }
+
+function WelcomeText(){
+    return(
+        <div className={style.welcomeText}>
+            <h1>Welcome to EdenCrate.</h1>
+            <p>Welcome to your online fruit store.Enjoy the freshest fruits delivered right to your doorstep.</p>
+        </div>
+    )
+}
+
+function ShopButton(){
+    return(
+        <Link to="Store" className={style.shopButton}>
+            <button>
+                Shop Now
+            </button>
+        </Link>
+    )
+}
+
+function Carousel(){
+    return(
+        <div className={style.carousel}>
+            <div className={style.mainPart}>
+
+            </div>
+            <div className={style.secondaryPart}>
+
+            </div>
+        </div>
+    )
+}
+
+
+
 
 export default HomePage;
