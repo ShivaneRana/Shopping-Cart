4ef6b48769772750bd749bad232aef84c02487c7
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-06-17 15:02:47 +0530

    1. added an outlet component as a placeholder for child routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import style from "./App.module.css";
 import Navbar from "./component/Navbar.jsx";
+import { Outlet } from "react-router-dom";
 
 function App() {
     return (
         <div className={style.main}>
             <Navbar></Navbar>
+            <Outlet></Outlet>
         </div>
     );
 }
