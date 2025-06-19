32cb84e803ba5a084d977f72192d7723a07e87f8
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-06-19 14:35:06 +0530

    basic structure created

---
+++ src/component/ProductShowcase.jsx
@@ -0,0 +1,11 @@
+import style from "../style/ProductShowcase.module.css";
+
+function ProductShowcase(){
+    return(
+        <div className={style.main}>
+
+        </div>
+    )
+}
+
+export default ProductShowcase;
---
+++ src/component/SideBar.jsx
@@ -0,0 +1,11 @@
+import style from "../style/SideBar.module.css";
+
+function SideBar(){
+    return(
+        <div className={style.main}>
+
+        </div>
+    )
+}
+
+export default SideBar;
