c6f04f5b2e079b0c8cf139067608d8b11a64f60f
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-06-19 16:19:15 +0530

    ColorSelector component partially completed

--- src/component/SideBar.jsx
+++ src/component/SideBar.jsx
@@ -1,10 +1,64 @@
 import style from "../style/SideBar.module.css";
+import expandedIcon from "../assets/images/down.svg";
+import shrinkIcon from "../assets/images/top.svg";
+import checkIcon from "../assets/images/check.svg";
+import { useState } from "react";
+import { uuidv7 } from "uuidv7";
 
 function SideBar(){
     return(
         <div className={style.main}>
-
+            <ColorSelector></ColorSelector>
         </div>
+    )
+}
+
+function ColorSelector(){
+    const [expanded,setExpanded] = useState(true);
+    const colorList = ["Red","Orange","Yellow","Green","Purple","Pink"];
+
+    function toggleExpanded(){
+        setExpanded((prev) => !prev);
+    }
+
+    return(
+        <div className={style.colorContainer}>
+            <div 
+            onClick={toggleExpanded}
+            className={style.colorHeadingHolder}>
+                <p>Color</p>
+                <img 
+                    title={expanded ? "Shrink section" : "Expand section"}
+                    src={expanded ? shrinkIcon : expandedIcon}></img>
+            </div>
+            <div className={style.colorItemHolder}>
+                {
+                    expanded ? colorList.map((item) => {
+                        const tempUUID = uuidv7();
+                        return <ColorItem key={tempUUID} color={item}></ColorItem>
+                    }) : null
+                }
+            </div>
+        </div>
+    )
+}
+
+function ColorItem({color}){
+    const [check,setCheck] = useState(false);
+    
+    function toggleCheck(){
+        setCheck((prev) => !prev);
+    }
+
+    return(
+        <div className={style.colorItem}>
+            <button onClick={toggleCheck} style={{backgroundColor:`${color}`}}>
+                {
+                    check ? <img src={checkIcon}></img> : null
+                }
+            </button>
+            <p>{color}</p>
+        </div> 
     )
 }
 
