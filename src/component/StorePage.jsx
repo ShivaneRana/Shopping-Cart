d55942a902a6490f6cd70f268b8ad4d25968071f
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-06-19 15:22:32 +0530

    SideBar and ProductShowcase component imported and called

--- src/component/StorePage.jsx
+++ src/component/StorePage.jsx
@@ -1,8 +1,12 @@
 import style from "../style/StorePage.module.css";
+import SideBar from "./SideBar.jsx";
+import ProductShowcase from "./ProductShowcase.jsx";
 
 function StorePage() {
     return (
         <div className={style.main}>
+            <SideBar></SideBar>
+            <ProductShowcase></ProductShowcase>
         </div>
     );
 }
