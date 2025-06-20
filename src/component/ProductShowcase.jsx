1ded05f8fe067e653c3076e123d7868ac69af793
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-06-20 22:14:35 +0530

    ProductShowcase component ui is completed

--- src/component/ProductShowcase.jsx
+++ src/component/ProductShowcase.jsx
@@ -1,7 +1,41 @@
 import style from "../style/ProductShowcase.module.css";
+import tempImage from "../assets/images/fruits/apple.png";
+import fruits from "../Fruits.jsx";
+import cartIcon from "../assets/images/cart.png";
+import cartPressedIcon from "../assets/images/cartPressed.png";
 
 function ProductShowcase() {
-    return <div className={style.main}></div>;
+    return <div className={style.main}>
+        <div className={style.headingHolder}>
+            <h2>Items ({fruits.length})</h2>
+        </div>
+        <div className={style.productHolder}>
+            {
+                fruits.map((item) => {
+                    return <ProductItem itemDetails={item} key={item.name}></ProductItem>
+                })
+            }
+        </div>
+    </div>;
 }
+
+function ProductItem({itemDetails}){
+    return(
+        <div className={style.productItem}>
+            <div className={style.imageHolder}>
+                <img src={tempImage}></img>
+            </div>
+            <div className={style.nameHolder}>
+                <h2>{itemDetails.name}</h2>
+                <h3>{itemDetails.family}</h3>
+            </div>
+            <div className={style.priceHolder}>
+                <h3>$ {itemDetails.price}</h3>
+                <img alt="Cart icon" src={cartIcon}></img>
+            </div>
+        </div>
+    )
+}
+
 
 export default ProductShowcase;
