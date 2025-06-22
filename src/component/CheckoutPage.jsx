81395c4675dc7091b85e483636dcaf68a1bced31
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-06-22 21:30:44 +0530

    removed placeholder heading

--- src/component/CheckoutPage.jsx
+++ src/component/CheckoutPage.jsx
@@ -3,7 +3,6 @@ import style from "../style/CheckoutPage.module.css";
 function CheckoutPage() {
     return (
         <div className={style.main}>
-            <h1>This is Checkout Page</h1>
         </div>
     );
 }
