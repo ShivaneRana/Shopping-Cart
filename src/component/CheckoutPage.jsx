2649d551c4784b78a86413b387f22cd530807527
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-06-22 22:43:03 +0530

    BackButton component added to go back to store from checkout page

--- src/component/CheckoutPage.jsx
+++ src/component/CheckoutPage.jsx
@@ -1,10 +1,25 @@
 import style from "../style/CheckoutPage.module.css";
+import backIcon from "../assets/images/back.svg";
+import { Link } from "react-router-dom";
 
 function CheckoutPage() {
     return (
         <div className={style.main}>
+            <div className={style.buttonHolder}>
+                <BackButton></BackButton>
+            </div>
         </div>
     );
+}
+
+function BackButton(){
+    return(
+        <Link to="/Shopping-Cart/Store">
+            <button className={style.backButton}>
+                <img title="Go back" alt="back icon" src={backIcon}></img>
+            </button>
+        </Link>
+    )
 }
 
 export default CheckoutPage;
