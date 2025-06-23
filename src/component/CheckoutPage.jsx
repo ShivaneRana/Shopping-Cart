5b2898275418908f0b8a8a57d461bfe4651caf40
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-06-23 20:26:23 +0530

    orderSummary and shoppingBag component completed

--- src/component/CheckoutPage.jsx
+++ src/component/CheckoutPage.jsx
@@ -1,8 +1,14 @@
 import style from "../style/CheckoutPage.module.css";
 import backIcon from "../assets/images/back.svg";
+import minusIcon from "../assets/images/minus.svg";
+import plusIcon from "../assets/images/plus.svg";
+import deleteIcon from "../assets/images/delete.png";
+import stockIcon from "../assets/images/inStock.svg";
 import { Link } from "react-router-dom";
+import fruits from "../Fruits.jsx";
 
 function CheckoutPage() {
+
     return (
         <div className={style.main}>
             <div className={style.buttonHolder}>
@@ -28,7 +34,67 @@ function BackButton() {
 }
 
 function ShoppingBag() {
-    return <div className={style.shoppingBag}></div>;
+
+    let start = 0;
+    let end = 10;
+
+    const list = fruits.splice(start,end);
+
+    return <div className={style.shoppingBag}>
+        {
+            list.map(item => {
+                const images = import.meta.glob("/src/assets/images/fruits/*.png", {
+                    eager: true,
+                    import: "default",
+                });
+
+                const name = item.name.toLowerCase();
+                const imageKey = `/src/assets/images/fruits/${name}.png`;
+                const src = images[imageKey];
+
+                return(
+                    <div className={style.orderContainer} key={item.name}>
+
+                        {/* contains the product image */}
+                        <div className={style.imageHolder}>
+                            <img src={src}></img>
+                        </div>
+
+                        {/* contains the product detail */}
+                        {/* name, family, quanitity */}
+                        <div className={style.itemDetail}>
+                            <h3 className={style.itemName}>{item.name}</h3>
+                            <h3 className={style.itemFamily}>{item.family} Family</h3>
+                            <div className={style.itemStock}>
+                                <img src={stockIcon}></img> 
+                                <p>In stock</p>
+                            </div>
+                            <h3 className={style.itemQuantity}>Qty: {item.quantity}</h3>
+                        </div>
+
+                        {/* increase/ decrease product quantity */}
+                        <div className={style.quantityController}>
+                            <button>
+                                <img src={minusIcon}></img>
+                            </button> 
+                            <p>{item.quantity}</p>
+                            <button>
+                                <img src={plusIcon}></img>
+                            </button>
+                        </div>
+
+                        {/*  display price and delete button */}
+                        <div className={style.priceDetail}>
+                            <button>
+                                <img src={deleteIcon}></img>
+                            </button>
+                            <h5>$ 90.90</h5>
+                        </div>
+                    </div>
+                )
+            })
+        }
+    </div>;
 }
 
 function OrderSummary() {
@@ -52,7 +118,7 @@ function OrderSummary() {
                     <h3>Total</h3>
                     <h3>$ 14.12</h3>
                 </div>
-                <button className={style.checkoutButton}>Checkout</button>
+                <button title = "" className={style.checkoutButton}>Checkout</button>
             </div>
         </div>
     );
