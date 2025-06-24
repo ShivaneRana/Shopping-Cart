33682b256ef2af1db7ac2c56b1a66f215b65af60
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-06-24 17:19:24 +0530

    passing cart and toggleCart from context api

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,40 @@
 import style from "./App.module.css";
 import Navbar from "./component/Navbar.jsx";
 import { createContext } from "react";
-import { Outlet } from "react-router-dom";
-import { useFavoriteState } from "./utility/Utility.jsx";
+import { Outlet,useLocation } from "react-router-dom";
+import { useFavoriteState,useCartState } from "./utility/Utility.jsx";
+import { useEffect } from "react";
 
 export let context = createContext();
 
 function App() {
     const { favourite, toggleFavourite } = useFavoriteState();
+    const {cart,toggleCart} = useCartState();
+    const location = useLocation();
+
+    console.log(location.pathname)
+    // toggle favourite to off when the url changes
+    // this is to ensure that the favourite state is off when not is store root.
+
+    useEffect(() => {
+        if(location.pathname !== "/Shopping-Cart/Store" && favourite){
+            toggleFavourite();
+        }
+
+        if(location.pathname !== "/Shopping-Cart/Checkout" && cart){
+            toggleCart();
+        }
+
+    },[location])
 
     return (
         <div className={style.main}>
             <context.Provider
                 value={{
                     favourite,
-                    toggleFavourite,
+                    cart,
+                    toggleCart,
+                    toggleFavourite
                 }}
             >
                 <Navbar></Navbar>
