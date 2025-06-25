732d181fd774f78480225f4006d7fbda099eefbe
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-06-25 16:11:11 +0530

    now dynamically render component based on url parameter

--- src/component/StorePage.jsx
+++ src/component/StorePage.jsx
@@ -1,12 +1,34 @@
 import style from "../style/StorePage.module.css";
 import SideBar from "./SideBar.jsx";
 import ProductShowcase from "./ProductShowcase.jsx";
+import fruits from "../Fruits.jsx";
+import ItemPage from "./ItemPage.jsx";
+import { useParams } from "react-router-dom";
 
 function StorePage() {
+    const {name} = useParams();
+    let itemDetail;
+
+    fruits.forEach(item => {
+        if(item.name === name){
+            itemDetail = item;
+        }
+    })
+
     return (
         <div className={style.main}>
-            <SideBar></SideBar>
-            <ProductShowcase></ProductShowcase>
+            {
+                // if param exists render item page
+                name !== undefined ? (
+                    <ItemPage itemDetail={itemDetail}></ItemPage>
+                ) : (
+                    // by default without any param render store
+                    <>
+                        <SideBar></SideBar>
+                        <ProductShowcase></ProductShowcase>
+                    </>
+                )
+            }
         </div>
     );
 }
