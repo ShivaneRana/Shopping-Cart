cf5c65a51693455cf61fec742e63084e4aa564f6
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-06-25 15:07:48 +0530

    basic boilerplate added

---
+++ src/component/ItemPage.jsx
@@ -0,0 +1,12 @@
+import style from "../style/ItemPage.module.css";
+
+function ItemPage({itemDetail}){
+
+    return(
+        <div className={style.main}>
+
+        </div>
+    )
+}
+
+export default ItemPage;
