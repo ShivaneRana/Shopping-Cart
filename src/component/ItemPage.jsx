c45cf8eebe32109774e9393cb37e86114c4bc329
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-06-25 15:08:04 +0530

    prettier used

--- src/component/ItemPage.jsx
+++ src/component/ItemPage.jsx
@@ -1,12 +1,7 @@
 import style from "../style/ItemPage.module.css";
 
-function ItemPage({itemDetail}){
-
-    return(
-        <div className={style.main}>
-
-        </div>
-    )
+function ItemPage({ itemDetail }) {
+    return <div className={style.main}></div>;
 }
 
 export default ItemPage;
