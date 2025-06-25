6f12ebc09984bc6d4dec609ea583cf7f85d7ee58
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-06-25 17:10:57 +0530

    logging target product

--- src/component/ItemPage.jsx
+++ src/component/ItemPage.jsx
@@ -2,6 +2,9 @@ import style from "../style/ItemPage.module.css";
 import { BackButton } from "./CheckoutPage";
 
 function ItemPage({ itemDetail }) {
+
+    console.log(itemDetail);
+
     return (
     <div className={style.main}>
             <div className={style.buttonHolder}>
