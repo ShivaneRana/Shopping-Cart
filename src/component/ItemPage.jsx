df1510faa066a25c6c1685ceac970ac65e320608
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-06-25 15:15:08 +0530

    Navbar imported

--- src/component/ItemPage.jsx
+++ src/component/ItemPage.jsx
@@ -1,7 +1,13 @@
 import style from "../style/ItemPage.module.css";
+import Navbar from "./Navbar";
 
 function ItemPage({ itemDetail }) {
-    return <div className={style.main}></div>;
+    return (
+    <div className={style.main}>
+        <Navbar></Navbar>
+        <h2>{itemDetail.name || "SHivane Rana was here"}</h2>
+    </div>
+    );
 }
 
 export default ItemPage;
